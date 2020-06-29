295022f0ae70baa7e01cffa0d5aa07ba268d27a0
Josh Wyant <joshwyant91@gmail.com>
2020-06-28 20:32:43 -0700

    Parallel build

--- build.sh
+++ build.sh
@@ -37,8 +37,8 @@ which -- $TARGET-as || echo $TARGET-as is not in the PATH
 mkdir build-gcc
 cd build-gcc
 ../gcc-8.1.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
-make all-gcc
-make all-target-libgcc
+make -j$((`nproc`+1)) all-gcc
+make -j$((`nproc`+1)) all-target-libgcc
 make install-gcc
 make install-target-libgcc
 
